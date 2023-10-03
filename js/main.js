0f367abde19dbe64929b1926ab8d9c924832afc2
NiRo <nyvl828@gmail.com>
2023-10-03 18:13:06 +0300

    js file - display img func mod

--- js/main.js
+++ js/main.js
@@ -15,17 +15,43 @@ function performSearch() {
         .catch(error => console.log(error));
 } 
 
-function displayImages(images) {
-    document.getElementById('loader').style.display = 'none';  // Hide loader once images are loaded
+// function displayImages(images) {
+//     document.getElementById('loader').style.display = 'none';  // Hide loader once images are loaded
+//     const imageContainer = document.getElementById('image-results');
+//     imageContainer.innerHTML = '';
+
+//     images.forEach(image => {
+//         const imageElement = document.createElement('img');
+//         imageElement.src = image.webformatURL;
+//         imageElement.className = 'image-card';
+
+//         imageContainer.appendChild(imageElement);
+//     });
+// }
+
+function displayImages(imageData) {
     const imageContainer = document.getElementById('image-results');
-    imageContainer.innerHTML = '';
-
-    images.forEach(image => {
-        const imageElement = document.createElement('img');
-        imageElement.src = image.webformatURL;
-        imageElement.className = 'image-card';
-
-        imageContainer.appendChild(imageElement);
+    imageContainer.innerHTML = '';  // Clear any existing content
+ 
+    imageData.forEach(image => {
+       const imageCard = document.createElement('div');
+       imageCard.classList.add('image-card');
+ 
+       const img = document.createElement('img');
+       img.src = image.webformatURL;
+       img.alt = image.tags;
+ 
+       const imageInfo = document.createElement('div');
+       imageInfo.classList.add('image-info');
+       imageInfo.innerHTML = `
+          <h3>${image.user}</h3>
+          <p>Tags: ${image.tags}</p>
+       `;
+ 
+       imageCard.appendChild(img);
+       imageCard.appendChild(imageInfo);
+ 
+       imageContainer.appendChild(imageCard);
     });
-}
-
+ }
+ 
